5b49fe3de8c528eb964e51e9f2805e5d7a92c4d5
Lautaro Garcia <lautarodanielgarcia15@gmail.com>
2024-09-23 22:23:34 -0300

    Subiendo ejercicios con cambios

--- EjTipoParcialCiclistas/src/Equipo.java
+++ EjTipoParcialCiclistas/src/Equipo.java
@@ -4,6 +4,7 @@ public class Equipo {
     float puntaje;
     int cantidadEquipo;
 
+
     public Equipo(String nombreEquipo, int tamanioDeEquipo) {
         this.nombreEquipo = nombreEquipo;
         this.equipo = new Ciclista[tamanioDeEquipo];
@@ -11,13 +12,16 @@ public class Equipo {
         this.cantidadEquipo = 0;
     }
 
+
     protected String getNombreEquipo() {
         return nombreEquipo;
     }
 
+
     protected void setNombreEquipo(String nombreEquipo) {
         this.nombreEquipo = nombreEquipo;
     }
+
 
     protected String agregarCiclista(Ciclista ciclista){
         if(cantidadEquipo<=equipo.length){
@@ -29,19 +33,22 @@ public class Equipo {
         }
     }
 
-    protected Ciclista listarCiclistas(){                                       //LISTA LOS CILISTAS Y TODOS SUS DATOS
+
+    protected Ciclista listarCiclistas(){                             //LISTA LOS CILISTAS Y TODOS SUS DATOS
         for(int i = 0; i<equipo.length && equipo[i] != null;i++){
             return equipo[i];
         }
         return null;
     }
 
+
     protected String listarNombresDeCiclistasPorEquipo(){
-        for(int i = 0; i<= equipo.length && equipo[i]!=null;i++){
+        for(int i = 0; equipo[i] != null; i++){
             return  equipo[i].getNombreCiclista();
         }
         return null;
     }
+
 
     protected String tiempoTotal(){
         int tiempoTotal = 0;
@@ -50,6 +57,7 @@ public class Equipo {
         }
         return "El tiempo total del equipo '"+ nombreEquipo +"' es: "+tiempoTotal;
     }
+
 
     protected String listarDatosCiclista(int identificador){
         for(int i = 0; i<= equipo.length && equipo[i] != null; i++){
