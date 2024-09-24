becf051c8248633a7d59b663aef380fead25e37f
Lautaro Garcia <lautarodanielgarcia15@gmail.com>
2024-09-23 23:40:47 -0300

    Subiendo ejercicios con cambios

--- EjFarmacia/src/Main.java
+++ EjFarmacia/src/Main.java
@@ -1,6 +1,25 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Medicamento medicamento1 = new DeLaboratorio("Medicamento1","dolor de estomago","Paracetamol",100, 20, "Abot");
+        Medicamento medicamento2 = new Generico("Medicamento2", "dolor de cabeza", "Diclofenac",100, 30,"Diclofenac");
+
+        medicamento1.aprobarMedicamento();
+
+        System.out.println(medicamento1.obtenerEstado());
+        System.out.println(medicamento2.obtenerEstado());
+
+        System.out.println("");
+
+        System.out.println("PRECIOS: ");
+        System.out.println(medicamento1.getPrecio());
+        System.out.println(medicamento2.getPrecio());
+
+        System.out.println("");
+
+        System.out.println("COMPRAS");
+        System.out.println(medicamento1.realizarCompra(5));
+        System.out.println(medicamento2.realizarCompra(40));
+
+
     }
 }
